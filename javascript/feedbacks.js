2b040ce0f16fb2c939f2dbb0f292b353ec409e81
JoaoGustavoMr <joaogustavo2202@gmail.com>
2024-10-03 16:11:38 -0300

    avanco feddback

--- javascript/feedbacks.js
+++ javascript/feedbacks.js
@@ -14,25 +14,18 @@ function menuShow() {
   if (menuMobile.classList.contains('open')) {
       menuMobile.classList.remove('open')
       document.querySelector('.icon').src = "../img/menu_white_36dp.svg"
-  }
-  else {
+  } else {
       menuMobile.classList.add('open')
       document.querySelector('.icon').src = "../img/close_white_36dp.svg"
   }
 }
+
 var exampleModal = document.getElementById('exampleModal')
 exampleModal.addEventListener('show.bs.modal', function (event) {
-  // Button that triggered the modal
-  var button = event.relatedTarget
-  // Extract info from data-bs-* attributes
-  var recipient = button.getAttribute('data-bs-whatever')
-  // If necessary, you could initiate an AJAX request here
-  // and then do the updating in a callback.
-  //
-  // Update the modal's content.
+
   var modalTitle = exampleModal.querySelector('.modal-title')
   var modalBodyInput = exampleModal.querySelector('.modal-body input')
 
-  modalTitle.textContent = 'New message to ' + recipient
-  modalBodyInput.value = recipient
+  modalTitle.textContent = 'Faça seu Depoimento'
+  modalBodyInput.value = ''
 })

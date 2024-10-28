2790ab6c6ce57a90fde4fc153b9f0a5e5a7e2637
Kaique <kaique1245br@gmail.com>
2024-10-27 23:23:22 -0300

    CARROSSEL FINALIZADO FALTA MELHORA AS SECTIONS

--- javascript/atracoes.js
+++ javascript/atracoes.js
@@ -1,37 +1,16 @@
-let currentIndex = 0;
+let index = 0;
 
-function slide(index) {
-    const slides = document.querySelector('.carousel-container');
-    const totalSlides = slides.children.length;
-    if (index >= totalSlides) {
-        currentIndex = 0;
-    } else if (index < 0) {
-        currentIndex = totalSlides - 1;
-    } else {
-        currentIndex = index;
+function move(direction) {
+    const items = document.querySelectorAll('.item');
+    const totalItems = items.length;
+    index += direction;
+
+    if (index < 0) {
+        index = totalItems - 1;
+    } else if (index >= totalItems) {
+        index = 0;
     }
-    slides.style.transform = `translateX(-${currentIndex * 600}px)`;
-}
 
-function nextSlide() {
-    slide(currentIndex + 1);
-}
-
-function prevSlide() {
-    slide(currentIndex - 1);
-}
-
-
-
-
-function menuShow() {
-    let menuMobile = document.querySelector('.mobile-menu');
-    if (menuMobile.classList.contains('open')) {
-        menuMobile.classList.remove('open')
-        document.querySelector('.icon').src = "../img/menu_white_36dp.svg"
-    }
-    else {
-        menuMobile.classList.add('open')
-        document.querySelector('.icon').src = "../img/close_white_36dp.svg"
-    }
+    const carrossel = document.querySelector('.carrossel');
+    carrossel.style.transform = `translateX(-${index * 25}%)`;
 }

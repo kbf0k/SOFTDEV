2aa2b9842002af9a9f8d454e1635e58738ea76d2
joaopedro-07 <jotapepe.machado@gmail.com>
2024-10-28 22:52:41 -0300

    melhora inicio com carrossel de grid

--- javascript/inicio.js
+++ javascript/inicio.js
@@ -9,3 +9,51 @@ function menuShow() {
         document.querySelector('.icon').src = "../img/close_white_36dp.svg"
     }
 }
+
+let slideIndex = 0;
+        let intervalo;
+
+        function mostrarSlides() {
+            const slides = document.querySelectorAll('.slide');
+            const indicadores = document.querySelectorAll('.indicador');
+            slides.forEach((slide, index) => {
+                slide.style.display = index === slideIndex ? 'block' : 'none';
+            });
+            indicadores.forEach((indicador, index) => {
+                indicador.classList.toggle('ativo', index === slideIndex);
+            });
+        }
+
+        function mudarSlide(n) {
+            slideIndex += n;
+
+            const slides = document.querySelectorAll('.slide');
+            if (slideIndex < 0) {
+                slideIndex = slides.length - 1;
+            } else if (slideIndex >= slides.length) {
+                slideIndex = 0;
+            }
+
+            mostrarSlides();
+            reiniciarCarrossel();
+        }
+
+        function mudarParaSlide(n) {
+            slideIndex = n;
+            mostrarSlides();
+            reiniciarCarrossel();
+        }
+
+        function iniciarCarrossel() {
+            intervalo = setInterval(() => {
+                mudarSlide(1);
+            }, 3000);
+        }
+
+        function reiniciarCarrossel() {
+            clearInterval(intervalo);
+            iniciarCarrossel();
+        }
+
+        iniciarCarrossel();
+        mostrarSlides();

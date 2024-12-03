9283f05aaa8039e88d29a99f630556c1106c4ae1
JoaoGustavoMr <joaogustavo2202@gmail.com>
2024-12-03 00:36:45 -0300

    tentei arrumar a responsividade e nao deu lixo

--- javascript/feedbacks.js
+++ javascript/feedbacks.js
@@ -136,4 +136,14 @@ document.getElementById('logout').addEventListener('click', () => {
         }
     });
 });
-
+function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open')
+        document.querySelector('.icon').src = "img/menu_white_36dp.svg"
+    }
+    else {
+        menuMobile.classList.add('open')
+        document.querySelector('.icon').src = "img/close_white_36dp.svg"
+    }
+}
